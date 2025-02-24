f3acbd2e1a42eb41bf092dc47fbd60655a1ade9e
Tiago-htm <tiagovilela019@gmail.com>
2025-02-24 17:04:28 -0300

    Adicionado função para salvar dados

--- main.py
+++ main.py
@@ -1,28 +1,62 @@
-#organizar em ordem alfabetica as biblioteca!!!!!!!!!!!!!!!!!!!!!!
-from fastapi import FastAPI
+#organizar em ordem alfabetica as biblioteca!
+from fastapi import FastAPI, HTTPException,Request,Depends
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
 from pydantic import BaseModel #pydantic serve para validação de dados
+from sqlalchemy.orm import Session
 from transformers import pipeline
-
+#Meus imports
+from database import get_db, engine
+from models import Base, SentimentAnalysis
 #------------------------------------------------------------------------------#
 
+#------------------------------------------------------------------------------#
+##Banco
+Base.metadata.create_all(bind=engine) 
 
+#------------------------------------------------------------------------------#
 app = FastAPI() #chama o Fast APi
 
-sentiment_analyzer =  pipeline("sentiment-analysis") # biblioteca que analisa sentimentos da transformers
+try:
+    sentiment_analyzer =  pipeline("sentiment-analysis") # biblioteca que analisa sentimentos da transformers
+except Exception as e:
+    raise HTTPException(status_code=500, detail = f"Erro ao carregar o modelo")
+
+
+
 
 #Essa class é basicamente um Struct em C
 class TextInput(BaseModel): #BaseModel é uma forma de Bolo, que vai garantir que o bolo vai sair redondo
         text: str #vai esperar resposta do tipo string
 
-@app.get("/")
-def home():
-    return {"message": "API de analise de Sentimentos"} #verifica se ta funcionando API
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=400,  
+        content={"detail": "Erro no formato de texto, Não esta em JSON"}
+    )
 
 @app.post("/analyze/")
 #Agora é possível receber um Json.
-def analyze_sentiment(input_data: TextInput): #input_data é do tipo TextInput
+def analyze_sentiment(input_data: TextInput, db: Session = Depends(get_db)): #input_data é do tipo TextInput
+ try:
     result = sentiment_analyzer(input_data.text) 
-    return{"text":input_data.text, 
-           "sentiment": result[0]["label"],
-           "confidence": round(result[0]["score"],  4)}
-    
+    sentiment = result[0]["label"]
+    confidence = round(result[0]["score"], 4)
+
+    sentiment_record = SentimentAnalysis(text=input_data.text,sentiment=sentiment, confidence=confidence)
+    db.add(sentiment_record)
+    db.commit() ## envia os dados para banco para salvar   
+    db.refresh(sentiment_record)
+    return{
+            "id": sentiment_record.id,
+            "text": sentiment_record.text,
+            "sentiment": sentiment_record.sentiment,
+            "confidence": sentiment_record.confidence,
+          }
+ 
+
+ except Exception as e:
+    raise HTTPException(status_code=500, detail = f"Erro ao processar a análise de sentimento.")
+
